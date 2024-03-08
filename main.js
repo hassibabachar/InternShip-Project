fea0b6b036133e84e6fa8f3ffd6bcf0dc3a81267
hassibabch <hassibabch28@gmail.com>
2024-03-08 23:28:31 +0100

    adding content

--- main.js
+++ main.js
@@ -1,18 +1,13 @@
-let openNav = document.querySelector(".open-nav") ;
-let closNav = document.querySelector(".close-nav") ;
-let nav = document.querySelector(".links") ;
-// opens nav dropdown 
-const open_nav =() =>{
-    nav.style.display = "flex"
-    openNav.style.display = "none"
-    closNav.style.display = "block"
-}
+let tabs = document.querySelectorAll(".links li a");
+let tabsArray = Array.from(tabs);
+let divs = document.querySelectorAll("section > div");
+let divsArray = Array.from(divs);
 
-// close nav dropdown 
-const closeNav =() =>{
-    closNav.style.display = "none"
-    nav.style.display = "none"
-    openNav.style.display = "block"
-}
-openNav.addEventListener('click',open_nav);
-closNav.addEventListener('click',closeNav);
+tabsArray.forEach((ele) => {
+    ele.addEventListener("click", function (e) {
+        tabsArray.forEach((ele) => {
+            ele.classList.remove("active");
+        });
+        e.currentTarget.classList.add("active");
+    });
+});
